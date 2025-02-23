9d3cc6b6344c25fc0a8dc31c21441b28aa03e766
motazmuhammad <motaz.muhammad@kaust.edu.sa>
2025-02-23 16:42:17 +0000

    fix a bug

--- LeetCode75/Asteroid Collision/main.cpp
+++ LeetCode75/Asteroid Collision/main.cpp
@@ -13,37 +13,44 @@ class Solution
          stack<int> s;
          for (auto& x : asteroids)
             {
-               if(x>0)
+            if (x > 0)
+               s.push(x);
+            else
+               {
+               if (s.empty())
                   s.push(x);
                else
                   {
-                  if(s.empty())
+                  auto current = s.top();
+                  if (current < 0)
                      s.push(x);
                   else
                      {
-                     auto current = s.top();
-                     if(current<0)
-                        s.push(x);
-                     else
-                        { 
-                        while (!s.empty()&& current>0 && current<abs(x))
-                           {
-                           s.pop();
-                           if(!s.empty())
-                              current = s.top();
-                           }
-                        if (!s.empty() && current == abs(x) )
-                           {
-                           s.pop();
-                           }
-                        if (!s.empty() && current < 0)
-                           {
-                           s.push(x);
-                           }
+                     while (!s.empty() && current > 0 && current < abs(x))
+                        {
+                        s.pop();
+                        if (!s.empty())
+                           current = s.top();
                         }
-                     }
+                     if (s.empty())
+                        {
+                        s.push(x);
+                        continue;
+                        }
+                     if (current < 0)
+                        {
+                        s.push(x);
+                        continue;
+                        }
+                     if (current == abs(x))
+                        {
+                        s.pop();
+                        }
 
+                     }
                   }
+
+               }
 
 
             }
@@ -59,7 +66,7 @@ class Solution
    };
 int main()
    {
-   vector<int> nums{ -2,-1,1,2 };
+   vector<int> nums{ 1,-2,-2,-2 };
    int k = 3;
    Solution sol;
    auto ret = sol.asteroidCollision(nums);
